5f85038f2730a22c8cb9d2f2fdcead9e3a068904
Rishabh Tripathi <devr1310@gmail.com>
2024-06-29 10:31:06 +0530

    add favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
-                >Rishabh's Portfolio</motion.a>
+                >Hello and Welcome!</motion.a>
                 <div className="flex gap-5">
                     <a target='_blank' href="https://github.com/rish1310"><img className='h-5' src="/github.png" alt="" /></a>
                     <a target='_blank' href="https://www.linkedin.com/in/rishabhtripathi13/"><img className='h-5' src="/linkedin.png" alt="" /></a>
